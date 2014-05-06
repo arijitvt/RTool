6ae1fc03a58322f94505ea5fab234c148767a4d0
Arijit Chattopadhyay <a>
2014-05-05 21:27:18 -0400

    Adding export.

--- exports.sh
+++ exports.sh
@@ -1,9 +1,10 @@
 
 #LLVM Latest compilation
 export PATH=/home/ari/cvs/src/bin:$PATH
-LLVM_INST_DIR=/home/ari/cvs/src/build/Release+Debug+Asserts/bin
+LLVM=/home/arijit/Research/RTool/build/Release+Debug+Asserts
+LLVM_INST_DIR=$LLVM/bin
 export PATH=$LLVM_INST_DIR:$PATH
-export llvm_lib=/home/ari/cvs/src/build/Release+Debug+Asserts/lib
+export llvm_lib=$LLVM_INST_DIR/lib
 export LD_LIBRARY_PATH=$llvm_lib:$LD_LIBRARY_PATH
-export dk=/home/ari/cvs/src/llvm-3.2.src/lib/Transforms/Daikon
+export dk=
 export CLASSPATH=/home/ari/cvs/src/SimpleDeclParser/parser.jar:$CLASSPATH
