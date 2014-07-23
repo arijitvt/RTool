7462e60281bd8d96ecbca4fc290861508c99eb22
Zsmith32 <Zsmith32@vt.edu>
2014-07-23 14:07:56 -0400

    modified hookAtFunctionStart and hookAtFunctionEnd 7/23 -Zach

--- llvm-3.2.src/lib/Transforms/Daikon/DaikonPass.h
+++ llvm-3.2.src/lib/Transforms/Daikon/DaikonPass.h
@@ -15,6 +15,7 @@
 #include "llvm/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/InstIterator.h"
+#include "llvm/DerivedTypes.h"
 
 #include "llvm/Support/CommandLine.h"
 
@@ -47,9 +48,6 @@ class DaikonPass:public ModulePass {
                 void hookAtFunctionEnd(Function *);
 		void  insertDynamicCallAtGlobalAccess(Function *);
                 
-		//hookForStore is not in use 
-		void hookForStore(Function *);
-                
 		//Program PointGeneration
 		void generateProgramPoints(Module &);
 		void loadProgramPoints(Module &);
@@ -70,13 +68,7 @@ class DaikonPass:public ModulePass {
 		Value*  getValueForString(StringRef,Module *);
 		string getTypeString(Value *v);
 		string getTypeString(Type *v);
-		string getRepString(string str);
 		void putTabInFile(fstream &,int);
-		bool isSupportedType(Value *v);
-		bool isSupportedType(Type *t);
-
-
-
 
 		//This is for testing
 		void testMethod(Function *);
@@ -90,20 +82,58 @@ class DaikonPass:public ModulePass {
 		void handleStoreInst(StoreInst *storeInst);
 		void blockBreaker(Instruction *);
 
+
+		//Get Rep Type String
+		string getRepTypeString(Value *val);
+		string getRepTypeString(Type *ty);
+
+		//Get Dec Type String
+		string getDecTypeString(Value *val);
+		string getDecTypeString(Type *ty);
+
+		//Type related  function.
+		bool isSupportedType(Value *val);
+		bool isSupportedType(Type *type);
+
+		//Dump the structure nexted members
+		void dumpStructureMembers(fstream &declFile,
+				Value *structElement,Type *ty,int tabCount,bool isGlobalStructure);
+
+		//Dump the various different types of Array
+		void dumpArrays(fstream &declFile,
+				Value *arrayElement, Type *ty,int tabCount,bool isGlobalArray);
+
+		//Get the type of the global variables 
+		//as they are by default pointer type 
+		Type* getGlobalType(PointerType *ty) ;
+
 	private:
 		vector<Value*>  globalList;
 		vector<string> 	doNotInstrumentFunctions;
 		vector<string> programPoints;
 		Type *voidType ;
 		IntegerType *int8Type ; 	
+		IntegerType *int16Type;
 		IntegerType *int32Type;
 		IntegerType *int64Type;
-		PointerType *ptr8Type ; 
+		
+		Type *floatType;
+		Type *doubleType;
+
+		StructType *structType;
+
+		PointerType *ptr8Type ;
+		PointerType *ptr16Type;
 		PointerType *ptr32Type;
 		PointerType *ptr64Type;
- 
+
+		PointerType *ptrFloatType;
+		PointerType *ptrDoubleType;
+
 		PointerType *ptrPtr32Type;
 		PointerType *ptrPtr64Type;
+
+		PointerType *ptrStructType;
 
 		FunctionType *functionType;
 		static bool isInit;
@@ -129,5 +159,22 @@ class DummyVarInsertionPass:public ModulePass {
 };
 
 
+
+//Primitive types
+static const string CHAR_TYPE 	= "char";
+static const string SHORT_TYPE 	= "short";
+static const string INT_TYPE 	= "int";
+static const string LONG_TYPE 	= "long";
+static const string DOUBLE_TYPE = "double";
+static const string FLOAT_TYPE  = "float";
+
+//Nested Types 
+static const string STRUCT_TYPE = "struct";
+
+//Sequential Types
+static const string ARRAY_TYPE 	= "array";
+
+//Pointer Type
+static const string POINTER_TYPE = "pointer";
 
 #endif
