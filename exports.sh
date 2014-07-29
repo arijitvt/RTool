c4f7c733818ce8f76e69c2569b084241e87c22a9
Zsmith32 <Zsmith32@vt.edu>
2014-07-29 16:20:42 -0400

    exports changed

--- exports.sh
+++ exports.sh
@@ -1,8 +1,8 @@
 #Bin path setting for the local bin
-export PATH=/home/arijit/Research/RTool/bin:$PATH
+export PATH=/home/zach/RTool/bin:$PATH
 
 #LLVM Latest compilation
-LLVM=/home/arijit/Research/RTool/build/Release+Debug+Asserts
+LLVM=/home/zach/RTool/build/Release+Debug+Asserts
 export PATH=$LLVM/bin:$PATH
 export llvm=$LLVM
 export llvm_bin=$LLVM/bin
@@ -10,13 +10,13 @@ export llvm_lib=$LLVM/lib
 export LD_LIBRARY_PATH=$llvm_lib:$LD_LIBRARY_PATH
 
 #Inspect PathSetting
-insp=/home/arijit/Research/RTool/inspect-0.3
+insp=/home/zach/RTool/inspect-0.3
 export insp
 export insp_lib=$insp/lib
 export insp_bin=$insp/bin
 
 #Daikon Path for hook.h
-export daikon=/home/arijit/Research/RTool/llvm-3.2.src/lib/Transforms/Daikon
+export daikon=/home/zach/RTool/llvm-3.2.src/lib/Transforms/Daikon
 
 #ClassPath setting for the parser jar
-export CLASSPATH=/home/arijit/Research/RTool/jars/parser.jar:$CLASSPATH
+export CLASSPATH=/home/zach/RTool/jars/parser.jar:$CLASSPATH
