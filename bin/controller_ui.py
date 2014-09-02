8e1dfd0fbd5841592e1ff9dbfa8ead649a59d173
Markus Kusano <mukusano@vt.edu>
2014-09-02 10:16:32 -0400

    add output to cleanup files.

--- bin/controller_ui.py
+++ bin/controller_ui.py
@@ -185,6 +185,7 @@ def all_clean():
         os.system("rm -rf daik_res");
         os.system("rm -f hook.c")
         os.system("rm -f ProgramPoints.ppts")
+        os.system("rm -f output")
         os.system("rm -f hook.h")
         os.system("rm -f program.dtrace")
         os.system("rm -f output.decl")
